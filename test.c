9696c2fa3a9a2e193e8821877a5701bd9b33077a
Ryoo <87628372+RyooHyun@users.noreply.github.com>
2021-10-13 19:45:22 +0900

    1to50

--- test.c
+++ test.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 void main() {
 	int sum = 0;
-	for(int i = 1; i <= 100; i++) {
+	for(int i = 1; i <= 50; i++) {
 		sum += i;
 	}
-	printf("1~100의 합은 %d 입니다.", sum);
+	printf("1~50의 합은 %d 입니다.", sum);
 }
